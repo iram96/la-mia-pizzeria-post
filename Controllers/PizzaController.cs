24509e0551e6076ad38025f44e79c618984c634e
iram96 <mariano.maselli@virgilio.it>
2022-06-03 20:36:19 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,58 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        //List<Pizza> pizzaList = new List<Pizza>();
+        public static pizzaList pizze;
+        public IActionResult Pizzeria()
+        {
+            pizze = new pizzaList();
+
+
+            Pizza Pizza1 = new Pizza("Margherita", "Pomodoro Mozzarella", 10);
+
+            Pizza Pizza2 = new Pizza("Marinara", "Pomodoro Origano", 10);
+
+            Pizza Pizza3 = new Pizza("Diavola", "Pomodoro Mozzarella Salame piccante", 15);
+
+
+            pizze.ListaPizze.Add(Pizza1);
+            pizze.ListaPizze.Add(Pizza2);
+            pizze.ListaPizze.Add(Pizza3);
+
+            return View(pizze);
+        }
+
+
+
+
+        public IActionResult CreaPizza()
+        {
+            Pizza MiaPizza = new Pizza()
+            {
+                Nome = "",
+                Desc = "",
+                Prezzo = 0
+            };
+            return View(MiaPizza);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreaSchedaPizza(Pizza DatiPizza)
+        {
+            Pizza MiaPizza = new Pizza()
+            {
+                Nome = DatiPizza.Nome,
+                Desc = DatiPizza.Desc,
+                Prezzo = DatiPizza.Prezzo
+            };
+            return View(DatiPizza);
+        }
+
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        [Required(ErrorMessage = "Il Nome è obbligatorio")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "La Descrizione è obbligatoria")]
+        [StringLength(10, ErrorMessage = "Più conciso dai")]
+        public string Desc { get; set; }
+
+        [Required(ErrorMessage = "Il Prezzo è obbligatorio")]
+
+        [Range(1, int.MaxValue, ErrorMessage = "Che fai, li paghi per fargli mangiare la pizza?")]
+        public int Prezzo { get; set; }
+        public Pizza(string Nome, string Desc, int Prezzo)
+        {
+            this.Nome = Nome;
+            this.Desc = Desc;
+            this.Prezzo = Prezzo;
+        }
+
+        public Pizza()
+        {
+        }
+    }
+
+    public class pizzaList
+    {
+        public List<Pizza> ListaPizze { get; set; }
+
+        public pizzaList()
+        {
+            ListaPizze = new();
+
+        }
+    }
+}
