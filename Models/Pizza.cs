57595dd93324112f6726d3880e690d76f60c897e
iram96 <mariano.maselli@virgilio.it>
2022-06-06 18:07:38 +0200

    Added show, edit and Delete of Pizza

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -4,19 +4,21 @@ namespace la_mia_pizzeria_static.Models
 {
     public class Pizza
     {
+        public int Id { get; set; }
         [Required(ErrorMessage = "Il Nome è obbligatorio")]
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
 
         [Required(ErrorMessage = "La Descrizione è obbligatoria")]
         [StringLength(10, ErrorMessage = "Più conciso dai")]
-        public string Desc { get; set; }
+        public string? Desc { get; set; }
 
         [Required(ErrorMessage = "Il Prezzo è obbligatorio")]
 
         [Range(1, int.MaxValue, ErrorMessage = "Che fai, li paghi per fargli mangiare la pizza?")]
-        public int Prezzo { get; set; }
-        public Pizza(string Nome, string Desc, int Prezzo)
+        public int? Prezzo { get; set; }
+        public Pizza(int Id, string Nome, string Desc, int Prezzo)
         {
+            this.Id = Id;
             this.Nome = Nome;
             this.Desc = Desc;
             this.Prezzo = Prezzo;
